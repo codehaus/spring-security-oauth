b32cb3743734c640c22ccdad5c1a70c2ee1338ba
stoicflame <stoicflame@e7ddcb7d-5748-0410-947e-ed6b0c7b9155>
2009-06-26 22:13:49 +0000

    tagging 3.0

---
+++ tags/spring-security-oauth-3.0/src/main/java/org/springframework/security/oauth/provider/callback/OAuthCallbackServices.java
@@ -0,0 +1,25 @@
+package org.springframework.security.oauth.provider.callback;
+
+/**
+ * Services for persisting a callback for a given request token.
+ *
+ * @author Ryan Heaton
+ */
+public interface OAuthCallbackServices {
+
+  /**
+   * Services used to store a callback url.
+   *
+   * @param callback The callback URL to store.
+   * @param requestToken The request token to which the callback is associated.
+   */
+  void storeCallback(String callback, String requestToken) throws OAuthCallbackException;
+
+  /**
+   * Read the callback for the specified request token.
+   *
+   * @param requestToken The request token.
+   * @return The callback URL.
+   */
+  String readCallback(String requestToken) throws OAuthCallbackException;
+}
