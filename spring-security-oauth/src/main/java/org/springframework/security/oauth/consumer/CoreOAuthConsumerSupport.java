84029e8e4fff9646d8b5eee9ddb9aad1e7ffa61d
stoicflame <stoicflame@e7ddcb7d-5748-0410-947e-ed6b0c7b9155>
2010-03-22 21:14:24 +0000

    oauthss-39

--- spring-security-oauth/src/main/java/org/springframework/security/oauth/consumer/CoreOAuthConsumerSupport.java
+++ spring-security-oauth/src/main/java/org/springframework/security/oauth/consumer/CoreOAuthConsumerSupport.java
@@ -365,7 +365,6 @@ public class CoreOAuthConsumerSupport implements OAuthConsumerSupport, Initializ
     if (!isAccessToken) {
       //create an empty token to make a request for a new unauthorized request token.
       requestToken = new OAuthConsumerToken();
-      requestToken.setNonce(getNonceFactory().generateNonce());
     }
 
     TreeMap<String, String> requestHeaders = new TreeMap<String, String>();
@@ -423,7 +422,6 @@ public class CoreOAuthConsumerSupport implements OAuthConsumerSupport, Initializ
     OAuthConsumerToken consumerToken = new OAuthConsumerToken();
     consumerToken.setValue(tokenValue);
     consumerToken.setSecret(tokenSecret);
-    consumerToken.setNonce(requestToken.getNonce());
     consumerToken.setResourceId(details.getId());
     consumerToken.setAccessToken(isAccessToken);
     return consumerToken;
@@ -483,7 +481,7 @@ public class CoreOAuthConsumerSupport implements OAuthConsumerSupport, Initializ
     }
 
     String tokenSecret = requestToken == null ? null : requestToken.getSecret();
-    String nonce = requestToken == null ? getNonceFactory().generateNonce() : requestToken.getNonce();
+    String nonce = getNonceFactory().generateNonce();
     oauthParams.put(OAuthConsumerParameter.oauth_consumer_key.toString(), Collections.singleton(details.getConsumerKey()));
     if ((requestToken != null) && (requestToken.getValue() != null)) {
       oauthParams.put(OAuthConsumerParameter.oauth_token.toString(), Collections.singleton(requestToken.getValue()));
