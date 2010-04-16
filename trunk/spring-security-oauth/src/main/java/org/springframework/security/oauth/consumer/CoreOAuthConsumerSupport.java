beae474a8fb35a9265390282b5a6c95773fb6dd8
stoicflame <stoicflame@e7ddcb7d-5748-0410-947e-ed6b0c7b9155>
2010-04-16 04:32:35 +0000

    3.15: redundancy in consumer base string calculation

--- trunk/spring-security-oauth/src/main/java/org/springframework/security/oauth/consumer/CoreOAuthConsumerSupport.java
+++ trunk/spring-security-oauth/src/main/java/org/springframework/security/oauth/consumer/CoreOAuthConsumerSupport.java
@@ -247,20 +247,25 @@ public class CoreOAuthConsumerSupport implements OAuthConsumerSupport, Initializ
    * @return The configured URL.
    */
   protected URL configureURLForProtectedAccess(URL url, OAuthConsumerToken requestToken, ProtectedResourceDetails details, String httpMethod, Map<String, String> additionalParameters) {
-    StringBuilder file = new StringBuilder(url.getPath());
-    if (!"POST".equalsIgnoreCase(httpMethod) && !"PUT".equalsIgnoreCase(httpMethod)) {
+    String file;
+    if (!"POST".equalsIgnoreCase(httpMethod) && !"PUT".equalsIgnoreCase(httpMethod) && !details.isAcceptsAuthorizationHeader()) {
+      StringBuilder fileb = new StringBuilder(url.getPath());
       String queryString = getOAuthQueryString(details, requestToken, url, httpMethod, additionalParameters);
-      file.append('?').append(queryString);
+      fileb.append('?').append(queryString);
+      file = fileb.toString();
+    }
+    else {
+      file = url.getFile();
     }
 
     try {
       if ("http".equalsIgnoreCase(url.getProtocol())) {
         URLStreamHandler streamHandler = getStreamHandlerFactory().getHttpStreamHandler(details, requestToken, this, httpMethod, additionalParameters);
-        return new URL(url.getProtocol(), url.getHost(), url.getPort(), file.toString(), streamHandler);
+        return new URL(url.getProtocol(), url.getHost(), url.getPort(), file, streamHandler);
       }
       else if ("https".equalsIgnoreCase(url.getProtocol())) {
         URLStreamHandler streamHandler = getStreamHandlerFactory().getHttpsStreamHandler(details, requestToken, this, httpMethod, additionalParameters);
-        return new URL(url.getProtocol(), url.getHost(), url.getPort(), file.toString(), streamHandler);
+        return new URL(url.getProtocol(), url.getHost(), url.getPort(), file, streamHandler);
       }
       else {
         throw new OAuthRequestFailedException("Unsupported OAuth protocol: " + url.getProtocol());
